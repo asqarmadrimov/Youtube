2748b0734ec1f258525cc8aadc8e0dce601be13a
asqarmadrimov <madrimov2530@gmail.com>
2022-10-24 16:14:51 +0300

    clon

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,7 +5,7 @@ import {categories} from '../utils/constants'
 const SideBar = ({selectedCategory, setSelectedCategory}) => {
 
   return (
-    <Stack direction="row" sx={{overflowY: "auto", height:{sx: "auto", md: "60%"}, flexDirection:{md: "column"} }}>
+    <Stack direction="row" sx={{overflowY: "auto", height:{sx: "auto", md: "90%"}, flexDirection:{md: "column"} }}>
       {categories.map((categorie) => (
         <button className='category-btn' onClick={() => setSelectedCategory(categorie.name)} style={{background: categorie.name === selectedCategory && '#FC1503', color: 'white'}} key={categorie.name}>
           <span style={{color: categorie.name === selectedCategory ? 'white' : 'red', marginRight: '20px'}} >{categorie.icon}</span>
